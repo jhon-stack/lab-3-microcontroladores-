3419bfd241fbe2f736172636aa9ffd4fd03d3aa5
jhon-stack <jgaitanaguilar@gmail.com>
2023-01-27 20:31:22 -0600

    primer commit seteando el repositorio

---
+++ src/lab_3/lab_3.ino
@@ -0,0 +1,17 @@
+/*Universidad de Costa Rica
+IE-0624 Laboratorio de Microcontroladores
+Prof: Marco Villalta Fallas
+Estudiantes: Jhon Gaitan Aguilar B73059 y Alex Varela 
+Laboratorio 3
+Voltimetro
+Ciclo: III-2022
+*/
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
