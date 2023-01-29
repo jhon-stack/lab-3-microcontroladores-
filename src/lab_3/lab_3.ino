bc8e27af5f52076e74200df8da5e6f0bc2a5c2e0
jhon-stack <jgaitanaguilar@gmail.com>
2023-01-28 23:31:02 -0600

    Estrucutra del Header. ignorar lab3.simu

--- src/lab_3/lab_3.ino
+++ src/lab_3/lab_3.ino
@@ -6,12 +6,38 @@ Laboratorio 3
 Voltimetro
 Ciclo: III-2022
 */
-void setup() {
-  // put your setup code here, to run once:
+////////////////////////////////////////////////////////////////////////////////
+//Includes:
+//Nota: Agregar esta libreria pudo hacerse agregando el zip dentro del IDE
+//      Yo lo hice agregando descomprimiendo ese zip y agregando manualmente el folder en el path </usr/share/arduino/libraries/>
+#include </usr/share/arduino/libraries/pcd8544-master/PCD8544.h>
+#include </usr/share/arduino/libraries/pcd8544-master/PCD8544.cpp>
+//Puede que hagan falta mas Includes.
+////////////////////////////////////////////////////////////////////////////////
+//Con respecto a la libreroia PCD8544, para que funciones se deben conectar a los pines del Arduino 1 (digital) de la siguiente manera:
+/*
+  No se exactamente donde esta definido esto, creo que es en PCD8544.cpp | PCD8544.h. Incluso se podrian cambiar
+  *RST  -> PIN[6]
+  *CS   -> PIN[7]
+  *CLK  -> PIN[3]
+  *DIN  -> PIN[4]
+  *D/C  -> PIN[5]
 
-}
+*/ 
 
-void loop() {
-  // put your main code here, to run repeatedly:
+static PCD8544 lcd;//
 
+void setup(){
+  //Analog I/O's
+
+  //Digital I/O's 
+  pinMode(8, OUPUT);//Pin del LED que se activa si el voltaje menor a -20V o superior a 20V
+  lcd.begin(84, 48);// PCD8544-compatible displays may have a different resolution...
+
+  //Serial communication
+  
+  }
+
+void loop(){
+ 
 }
